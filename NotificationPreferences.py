322069dd7a89043a88d0e50222091409bce1fe9c
Maxwell Yang <yangmax@oregonstate.edu>
2025-02-21 18:04:41 -0800

    finished initial attributes

--- NotificationPreferences.py
+++ NotificationPreferences.py
@@ -1,4 +1,4 @@
 class NotificationPreferences:
-    
     #NotificationPreferences stores a user's notification settings
-    
+    frequency = "daily"  #e.g. daily, weekly
+    is_enabled = True    #bool
